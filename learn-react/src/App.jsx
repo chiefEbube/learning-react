e85876b27359e367b70651cc0791e669941ca26f
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-05-28 14:30:22 +0100

    created first components

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import NavBar from "./components/NavBar";
 import MainContent from './components/MainContent';
 
-export default function App(){
-  return(
-    <>
-    <MainContent/>
-    <NavBar />
-    </>
+
+export default function App() {
+  return (
+    <div className='container'>
+      <NavBar />
+      <MainContent />
+    </div>
   )
 }
--- learn-react/src/components/NavBar.jsx
+++ learn-react/src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import logo from '../assets/react-logo.png';
 
-export default function NavBar(){
-    return(
-        <h1>Nav bar goes here</h1>
+export default function NavBar() {
+    return (
+        <div className='navbar'>
+            <div className='logoContainer'>
+                <img src={logo} alt="image" />
+                <h3>ReactFacts</h3>
+            </div>
+
+            <h4>React Course - Project 1</h4>
+        </div>
     )
 }
