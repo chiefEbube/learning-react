c9a3c5910c8d9e06cea72b8ebf16e870e485cb94
chiefEbube <ebubechi.anyanwu@yahoo.com>
2023-06-13 07:25:26 +0100

    added props

--- air-bnb-experiences-clone/src/App.jsx
+++ air-bnb-experiences-clone/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import './App.css'
 import HeroSection from './components/HeroSection'
+import Card from './components/Card'
+import katieZaferes from './assets/images/katie-zaferes.png'
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <HeroSection/>
+      <NavBar />
+      <HeroSection />
+      <Card
+        img={katieZaferes}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136} />
     </>
   )
 }
